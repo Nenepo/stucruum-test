1e15ce8a9230f971cabfde6140cffee102ee06dd
Muhammad Said <mohammedabdy10@gmail.com>
2024-08-26 08:33:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
 
         <Route path="/" element={<Booking />} />
-        <Route path="/home" element={<ATMCreativity />} />
+        <Route path="/atm-creativity" element={<ATMCreativity />} />
       </Routes>
     </Router>
   );
